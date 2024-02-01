15865f783f70a18f66192e98f9d2173cdbde9f40
Tomas3371~ <Tomasdavid337@seznam.cz>
2024-02-01 21:18:01 +0100

    Added topbar, floating action button and declared classes storing the data about sets and cards

--- app/src/main/java/tomasdavid/flashcards2/MainActivity.kt
+++ app/src/main/java/tomasdavid/flashcards2/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import tomasdavid.flashcards2.ui.theme.Flashcards2Theme
 
@@ -20,23 +31,70 @@ class MainActivity : ComponentActivity() {
         setContent {
             Flashcards2Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    MainTopAppBar()
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.surface) {
+                    MainScreen()
                 }
             }
         }
     }
 }
 
+class Card(
+    val text1: String,
+    val text2: String,
+    val cardImg: String = "",  // TODO add default img path
+)
+
+class Set(
+    val setName: String,
+    val setImg: String = "",  // TODO add default img path
+) {
+    var cards = mutableListOf<Card>()
+    val displayOrder = listOf(listOf("text1", "cardImg"), listOf("text2"), listOf())
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainTopAppBar() {
-    TopAppBar(
-        title = {
-            Text(text = "Sets")
+fun MainScreen() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+//    var sets by remember { mutableStateListOf<Set>() }  // TODO save instance state
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Sets")
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                ),
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                containerColor = MaterialTheme.colorScheme.secondary,
+                contentColor = MaterialTheme.colorScheme.onSecondary
+            ) {
+                Icon(Icons.Filled.Add, "add")
+            }
         }
-    )
+    ) { innerPadding ->
+        // TODO display sets
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            items(100) {
+                Text(text="$it")
+            }
+        }
+    }
 }
 
 @Composable
